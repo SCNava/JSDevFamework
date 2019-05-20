dae6ca4b918b533b68d8e08ed252282a8797f9c2
Steven Nava <steven.nava@trojans.dsu.edu>
2019-05-19 20:26:31 -0400

    added test breakpoint to test debugger and sourcemaps

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0,00');
+//debugger; //this is a breakpoint
 console.log(`Is this course worth ${courseValue}?`);
